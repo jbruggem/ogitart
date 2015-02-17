0767fdd1b517cc5fb8878121326e80d51fc9a991
Jehan Bruggeman <jehan.bruggeman@euranova.eu>
2015-02-17 13:42:03 +0100

    refactorings

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+rm -rf ./node_modules/ ./build
+npm install --production
 npm run-script build
 npm run-script start
--- src/main.js
+++ src/main.js
@@ -3,23 +3,29 @@ var bodyParser = require("body-parser");
 
 var config = require('./config');
 var routes = require('./routes');
-var db = new require('./db').Database(config);
+var Database = require('./db').Database;
 
 
 
-function main(port){
+function main(config){
     var app = express();
     app.use(bodyParser.json());
 
     routes.routeIndex(app);
     routes.routeRessources(app);
+    var db = new Database(config);
     routes.routeData(app, db);
 
-    var server = app.listen(port, function(){
-      console.log('App listening at http://%s:%s', server.address().address, port);
+    var server = app.listen(config.port, function(){
+      console.log('App listening at http://%s:%s', server.address().address, config.port);
     });
 }
 
 
+if (require.main === module) {
+    main(config);
+}
 
-main(config.port);
+module.exports = {
+    main: main,
+}
