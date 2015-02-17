ac91deed253be661b18ddfc3b0cc5fd0d4d92fc6
Jehan Bruggeman <jehan.bruggeman@euranova.eu>
2015-02-17 12:30:20 +0100

    add tests

---
+++ test/index.js
@@ -0,0 +1,58 @@
+var should = require("should");
+
+describe('utils', function(){
+    var utils = require('../src/utils');
+    describe('nth', function(){
+        it('should return the nth elements of the list of lists', function(){
+            ['aa', 'cde', 'eyu'].should.eql(utils.nth([['aa', 'bd'], ['cde', 'd'], ['eyu', 'f'] ], 0));
+        });
+    });
+
+    describe('comparer', function(){
+        it('should prepare string for compare', function(){
+            "abdc efg f ft trudd v".should.eql(utils.comparer("   --$$abDc Efg- f ; + fT trUdd v  "));
+        });
+    });
+
+    describe('capitalize', function(){
+        it('should capitalize', function(){
+            "Abdc Efgft Truv".should.eql(utils.capitalize("abDc EfgfT trUv"));
+        });
+    });
+
+    describe('normalizeUserName', function(){
+        it('should normalize', function(){
+            "Ab C T'est Plo-up".should.eql(utils.normalizeUserName("-Ab  c; t'EsT plo-uP"));
+        });
+        it('should keep all-caps', function(){
+            "ABC".should.eql(utils.normalizeUserName("ABC"));
+        });
+    });
+
+    describe('normalizeDishName', function(){
+        it('should normalize', function(){
+            "-ab C; T'est Plo-up".should.eql(utils.normalizeDishName("-Ab  c; t'EsT plo-uP"));
+        });
+        it('should not keep all-caps', function(){
+            "Abc".should.eql(utils.normalizeDishName("ABC"));
+        });
+    });
+
+    describe('normalizeUserNames', function(){
+        it('should return a modified choices array', function(){
+            ['Abd', 'ZEF', 'Qrt'].should.eql(utils.normalizeUserNames([['ABd', 'bbb'], ['ZEF', 'dddd'], ['  qrt', 'ffff']]));
+        });
+    });
+
+    describe('normalizeDishNames', function(){
+        it('should return a modified choices array', function(){
+            ['Abd', 'Zef', 'Qrt'].should.eql(utils.normalizeDishNames([['bbb', 'ABd'], ['dddd', 'ZEF'], ['ffff', '  qrt']]));
+        });
+    });
+
+    describe('normalizeChoices', function(){
+        it('should return a modified choices array', function(){
+            [[ 'Bbb', 'Abd'], ['Dddd', 'Zef'], ['Ffff', 'Qrt']].should.eql(utils.normalizeChoices([['bbb', 'ABd'], ['dddd', 'ZEF'], ['ffff', '  qrt']]));
+        });
+    });
+});
